9e20a2f6fe66f44f6aa200d0dc21f7bcd340b22b
kkumarasamy <kkumarasamy@edit-place.com>
2019-01-08 12:11:25 +0530

    Added top menu toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
+    <v-navigation-drawer dark v-model="sideNav" class="teal">
+      <v-list>
+        <v-list-tile avatar>
+          <v-list-tile-action>
+            <v-icon left>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app dark color="teal darken-4">
+      <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Vue DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left dark>supervisor_account</v-icon>View Meetups
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <main></main>
   </v-app>
 </template>
 
 <script>
-  import HelloWorld from "./components/HelloWorld";
-
   export default {
     name: "App",
-    components: {
-      HelloWorld
-    },
     data() {
       return {
-        //
+        sideNav: false
       };
     }
   };
