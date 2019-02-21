a99525c156164eeee2b5eb5319a9800380760f3c
kathirr007 <kkumarasamy@edit-place.com>
2019-02-21 12:58:47 +0530

    Initial dist subtree commit

---
+++ dist/precache-manifest.9b4bb5448be91feafe770f89e1d72f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b23f9bfc446a6aa4d77d",
+    "url": "/js/chunk-vendors.c0c3d0d9.js"
+  },
+  {
+    "revision": "30806883c908f952e91d",
+    "url": "/js/app.7b124832.js"
+  },
+  {
+    "revision": "292e9eed6bcde721c93c2fa1d060d5c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23f9bfc446a6aa4d77d",
+    "url": "/css/chunk-vendors.df902753.css"
+  },
+  {
+    "revision": "30806883c908f952e91d",
+    "url": "/css/app.6f87d163.css"
+  }
+];
