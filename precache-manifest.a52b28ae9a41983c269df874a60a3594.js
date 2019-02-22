f71fca2dfb3d92d0e84587300aba5733cad52af0
kkumarasamy <kkumarasamy@edit-place.com>
2019-02-22 19:27:20 +0530

    deploy

---
+++ precache-manifest.a52b28ae9a41983c269df874a60a3594.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-devmeetup/robots.txt"
+  },
+  {
+    "revision": "50bb338882bde7f3f05f",
+    "url": "/vue-devmeetup/js/chunk-vendors.cc32bfdc.js"
+  },
+  {
+    "revision": "b83f296478bd7b916099",
+    "url": "/vue-devmeetup/js/app.f00d8379.js"
+  },
+  {
+    "revision": "c9bd482b7d276afc07f3c5c4e09687ef",
+    "url": "/vue-devmeetup/index.html"
+  },
+  {
+    "revision": "50bb338882bde7f3f05f",
+    "url": "/vue-devmeetup/css/chunk-vendors.fd57c053.css"
+  },
+  {
+    "revision": "b83f296478bd7b916099",
+    "url": "/vue-devmeetup/css/app.2545e08d.css"
+  },
+  {
+    "revision": "d405e178e163f345f1729cc0e68f36ce",
+    "url": "/vue-devmeetup/404.html"
+  }
+];
